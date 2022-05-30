91e8d8694e83501776e017f8de624058a23e26ff
stepylopex <83437121+stepylopex@users.noreply.github.com>
2022-05-30 12:29:49 +0800

    Add files via upload

---
+++ ListView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MOD04_PDC03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListView : ContentPage
+    {
+        public ListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
